5884c073b088fcdeb7f2fedab7596214a36b02c3
danco <daniel.coombs@mohawkcollege.ca>
2024-10-03 20:28:42 -0400

    colours and x + y float coordinates added

--- Coombs-Daniel-a2-drawing/Game.cs
+++ Coombs-Daniel-a2-drawing/Game.cs
@@ -11,7 +11,13 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        Color grayBuilding = new Color(0x4b, 0x4a, 0x4b);
+        Color grayWindow = new Color(0x6c, 0x6a, 0x6c);
+        Color yellowWindow = new Color(0xff, 0xdf, 0x00);
+        Color nightSkyBlue = new Color(0x08, 0x08, 0x38);
 
+        float[] xCoordinates = [];
+        float[] yCoordinates = [];
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
