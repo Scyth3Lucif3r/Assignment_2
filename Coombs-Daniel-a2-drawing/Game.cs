1422052f413304c4c9eb963829d5028646731665
danco <daniel.coombs@mohawkcollege.ca>
2024-10-07 13:42:30 -0400

    Various window Experimenting.

--- Coombs-Daniel-a2-drawing/Game.cs
+++ Coombs-Daniel-a2-drawing/Game.cs
@@ -29,7 +29,10 @@ namespace Game10003
 
         float[,] windowX = new float[6, 8];
         float[,] windowY = new float[6, 8];
-        bool[] isOnWindow = new bool[48];
+        float[,] isOnWindow = new float[6, 8];
+
+        int num1;
+        int num2;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -64,9 +67,16 @@ namespace Game10003
                 } 
 
             }
-            
+            Console.WriteLine(isOnWindow[num1, num2]);
             Draw.LineSize = 0;
-            Draw.FillColor = grayBuilding;
+            if (isOnWindow[num1, num2] > 0 && isOnWindow[num1, num2] < 3)
+            {
+                Draw.FillColor = yellowWindow;
+            } else if (isOnWindow[num1, num2] <= 3 || isOnWindow[num1, num2] <= 0)
+            {
+                Draw.FillColor = grayBuilding;
+                isOnWindow[num1, num2] = 0;
+            }
             // Horizontal building rectangles
             Draw.Rectangle(80, 60, 240, 30);
             for (int index = 0; index < 8; index++)
@@ -102,67 +112,73 @@ namespace Game10003
             //Change Window Color
             xMouse = Input.GetMouseX();
             yMouse = Input.GetMouseY();
-            turnOnWindow(xMouse, yMouse, 90, 110, 90, 110, 0);
-            turnOnWindow(xMouse, yMouse, 90, 110, 130, 150, 1);
-            turnOnWindow(xMouse, yMouse, 90, 110, 170, 190, 2);
-            turnOnWindow(xMouse, yMouse, 90, 110, 210, 230, 3);
-            turnOnWindow(xMouse, yMouse, 90, 110, 250, 270, 4);
-            turnOnWindow(xMouse, yMouse, 90, 110, 290, 310, 5);
-            turnOnWindow(xMouse, yMouse, 90, 110, 330, 350, 6);
-            turnOnWindow(xMouse, yMouse, 90, 110, 370, 390, 7);
-            turnOnWindow(xMouse, yMouse, 130, 150, 90, 110, 8);
-            turnOnWindow(xMouse, yMouse, 130, 150, 130, 150, 9);
-            turnOnWindow(xMouse, yMouse, 130, 150, 170, 190, 10);
-            turnOnWindow(xMouse, yMouse, 130, 150, 210, 230, 11);
-            turnOnWindow(xMouse, yMouse, 130, 150, 250, 270, 12);
-            turnOnWindow(xMouse, yMouse, 130, 150, 290, 310, 13);
-            turnOnWindow(xMouse, yMouse, 130, 150, 330, 350, 14);
-            turnOnWindow(xMouse, yMouse, 130, 150, 370, 390, 15);
-            turnOnWindow(xMouse, yMouse, 170, 190, 90, 110, 16);
-            turnOnWindow(xMouse, yMouse, 170, 190, 130, 150, 17);
-            turnOnWindow(xMouse, yMouse, 170, 190, 170, 190, 18);
-            turnOnWindow(xMouse, yMouse, 170, 190, 210, 230, 19);
-            turnOnWindow(xMouse, yMouse, 170, 190, 250, 270, 20);
-            turnOnWindow(xMouse, yMouse, 170, 190, 290, 310, 21);
-            turnOnWindow(xMouse, yMouse, 170, 190, 330, 350, 22);
-            turnOnWindow(xMouse, yMouse, 170, 190, 370, 390, 23);
-            turnOnWindow(xMouse, yMouse, 210, 230, 90, 110, 24);
-            turnOnWindow(xMouse, yMouse, 210, 230, 130, 150, 25);
-            turnOnWindow(xMouse, yMouse, 210, 230, 170, 190, 26);
-            turnOnWindow(xMouse, yMouse, 210, 230, 210, 230, 27);
-            turnOnWindow(xMouse, yMouse, 210, 230, 250, 270, 28);
-            turnOnWindow(xMouse, yMouse, 210, 230, 290, 310, 29);
-            turnOnWindow(xMouse, yMouse, 210, 230, 330, 350, 30);
-            turnOnWindow(xMouse, yMouse, 210, 230, 370, 390, 31);
-            turnOnWindow(xMouse, yMouse, 250, 270, 90, 110, 32);
-            turnOnWindow(xMouse, yMouse, 250, 270, 130, 150, 33);
-            turnOnWindow(xMouse, yMouse, 250, 270, 170, 190, 34);
-            turnOnWindow(xMouse, yMouse, 250, 270, 210, 230, 35);
-            turnOnWindow(xMouse, yMouse, 250, 270, 250, 270, 36);
-            turnOnWindow(xMouse, yMouse, 250, 270, 290, 310, 37);
-            turnOnWindow(xMouse, yMouse, 250, 270, 330, 350, 38);
-            turnOnWindow(xMouse, yMouse, 250, 270, 370, 390, 39);
-            turnOnWindow(xMouse, yMouse, 290, 310, 90, 110, 40);
-            turnOnWindow(xMouse, yMouse, 290, 310, 130, 150, 41);
-            turnOnWindow(xMouse, yMouse, 290, 310, 170, 190, 42);
-            turnOnWindow(xMouse, yMouse, 290, 310, 210, 230, 43);
-            turnOnWindow(xMouse, yMouse, 290, 310, 250, 270, 44);
-            turnOnWindow(xMouse, yMouse, 290, 310, 290, 310, 45);
-            turnOnWindow(xMouse, yMouse, 290, 310, 330, 350, 46);
-            turnOnWindow(xMouse, yMouse, 290, 310, 370, 390, 47);
+            //Row 1
+            turnOnWindow(xMouse, yMouse, 90, 110, 90, 110, 0, 0);
+            turnOnWindow(xMouse, yMouse, 90, 110, 130, 150, 0, 1);
+            turnOnWindow(xMouse, yMouse, 90, 110, 170, 190, 0, 2);
+            turnOnWindow(xMouse, yMouse, 90, 110, 210, 230, 0, 3);
+            turnOnWindow(xMouse, yMouse, 90, 110, 250, 270, 0, 4);
+            turnOnWindow(xMouse, yMouse, 90, 110, 290, 310, 0, 5);
+            turnOnWindow(xMouse, yMouse, 90, 110, 330, 350, 0, 6);
+            turnOnWindow(xMouse, yMouse, 90, 110, 370, 390, 0, 7);
+            //Row 2
+            turnOnWindow(xMouse, yMouse, 130, 150, 90, 110, 1, 0);
+            turnOnWindow(xMouse, yMouse, 130, 150, 130, 150, 1, 1);
+            turnOnWindow(xMouse, yMouse, 130, 150, 170, 190, 1, 2);
+            turnOnWindow(xMouse, yMouse, 130, 150, 210, 230, 1, 3);
+            turnOnWindow(xMouse, yMouse, 130, 150, 250, 270, 1, 4);
+            turnOnWindow(xMouse, yMouse, 130, 150, 290, 310, 1, 5);
+            turnOnWindow(xMouse, yMouse, 130, 150, 330, 350, 1, 6);
+            turnOnWindow(xMouse, yMouse, 130, 150, 370, 390, 1, 7);
+            //Row 3
+            turnOnWindow(xMouse, yMouse, 170, 190, 90, 110, 2, 0);
+            turnOnWindow(xMouse, yMouse, 170, 190, 130, 150, 2, 1);
+            turnOnWindow(xMouse, yMouse, 170, 190, 170, 190, 2, 2);
+            turnOnWindow(xMouse, yMouse, 170, 190, 210, 230, 2, 3);
+            turnOnWindow(xMouse, yMouse, 170, 190, 250, 270, 2, 4);
+            turnOnWindow(xMouse, yMouse, 170, 190, 290, 310, 2, 5);
+            turnOnWindow(xMouse, yMouse, 170, 190, 330, 350, 2, 6);
+            turnOnWindow(xMouse, yMouse, 170, 190, 370, 390, 2, 7);
+            //Row 4
+            turnOnWindow(xMouse, yMouse, 210, 230, 90, 110, 3, 0);
+            turnOnWindow(xMouse, yMouse, 210, 230, 130, 150, 3, 1);
+            turnOnWindow(xMouse, yMouse, 210, 230, 170, 190, 3, 2);
+            turnOnWindow(xMouse, yMouse, 210, 230, 210, 230, 3, 3);
+            turnOnWindow(xMouse, yMouse, 210, 230, 250, 270, 3, 4);
+            turnOnWindow(xMouse, yMouse, 210, 230, 290, 310, 3, 5);
+            turnOnWindow(xMouse, yMouse, 210, 230, 330, 350, 3, 6);
+            turnOnWindow(xMouse, yMouse, 210, 230, 370, 390, 3, 7);
+            //Row 5
+            turnOnWindow(xMouse, yMouse, 250, 270, 90, 110, 4, 0);
+            turnOnWindow(xMouse, yMouse, 250, 270, 130, 150, 4, 1);
+            turnOnWindow(xMouse, yMouse, 250, 270, 170, 190, 4, 2);
+            turnOnWindow(xMouse, yMouse, 250, 270, 210, 230, 4, 3);
+            turnOnWindow(xMouse, yMouse, 250, 270, 250, 270, 4, 4);
+            turnOnWindow(xMouse, yMouse, 250, 270, 290, 310, 4, 5);
+            turnOnWindow(xMouse, yMouse, 250, 270, 330, 350, 4, 6);
+            turnOnWindow(xMouse, yMouse, 250, 270, 370, 390, 4, 7);
+            //Row 6
+            turnOnWindow(xMouse, yMouse, 290, 310, 90, 110, 5, 0);
+            turnOnWindow(xMouse, yMouse, 290, 310, 130, 150, 5, 1);
+            turnOnWindow(xMouse, yMouse, 290, 310, 170, 190, 5, 2);
+            turnOnWindow(xMouse, yMouse, 290, 310, 210, 230, 5, 3);
+            turnOnWindow(xMouse, yMouse, 290, 310, 250, 270, 5, 4);
+            turnOnWindow(xMouse, yMouse, 290, 310, 290, 310, 5, 5);
+            turnOnWindow(xMouse, yMouse, 290, 310, 330, 350, 5, 6);
+            turnOnWindow(xMouse, yMouse, 290, 310, 370, 390, 5, 7); 
 
 
-            void turnOnWindow (float xMouse, float yMouse, float windowX_checkMin, float windowX_checkMax, float windowY_checkMin, float windowY_checkMax, int num)
+            void turnOnWindow (float xMouse, float yMouse, float windowX_checkMin, float windowX_checkMax, float windowY_checkMin, float windowY_checkMax, int num1, int num2)
             {
                 if (xMouse >= windowX_checkMin && xMouse <= windowX_checkMax)
                 {
-                    if ((yMouse >= windowY_checkMin && yMouse <= windowY_checkMax) || isOnWindow[0] == true)
+                    if ((yMouse >= windowY_checkMin && yMouse <= windowY_checkMax) || (isOnWindow[num1, num2] == 1 || isOnWindow[num1, num2] == 2))
                     {
                         Draw.FillColor = yellowWindow;
-                        Draw.Square(windowX[num, num], windowY[num, num], 20);
-                        isOnWindow[0] = !isOnWindow[0];
-                        Thread.Sleep(100);
-                        Console.WriteLine(isOnWindow[0]);
+                        Draw.Square(windowX[num1, num2], windowY[num1, num2], 20);
+                        isOnWindow[num1, num2] += 1;
+                        Console.WriteLine(isOnWindow[num1, num2]);
+                        
 
                     }
                 }
